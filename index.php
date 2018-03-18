607469d427e85adf55fdd51e726c5a6144060edf
jo <jo>
2018-03-19 01:06:59 +0530

    first

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<body bgcolor="#FFFF0F"><center>PRODUCTION</center>
+<br><br>
+hi
+<center>
+<?php echo "HI ALL" ?><center>
+<br><br>
+<center>
+<?php echo getenv("environment"); ?>
+</center>
+</body>
+</html>
